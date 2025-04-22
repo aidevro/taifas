bf15f1e1a446fec40d830ad63dfbc798c5c12468
Marius <marius.bugaciu@gmail.com>
2025-04-22 05:28:46 +0000

    new updates

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import './index.css';
 import Home from "./index.jsx";
 import Dashboard from "./Dashboard.jsx";
 import Meeting from "./components/Meeting.jsx"; // 👈 make sure this path is correct
